d088ce81a6f2e1f29d52ec0166d50cf039af9394
Aravind33333 <naravind105@gmail.com>
2026-02-04 13:26:27 +0530

    Updated the merger from Branch

--- backend/main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get("/")
-def check_status():
-    print("I am a simple print statement inside the route.") # This goes to file now!
-    return {"status": "API is running"}
